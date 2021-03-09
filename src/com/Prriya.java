f674d91f490f28e30a3fc97afe427f5d1c8f334b
anu <agrawalanupriya24@gmail.com>
2021-03-09 18:18:42 +0530

    Adding Prriya

---
+++ src/com/Prriya.java
@@ -0,0 +1,14 @@
+package com;
+
+public class Prriya
+{
+    public static void main(String[] args) {
+
+        System.out.println("HelloR");
+        System.out.println("HelloR");
+        System.out.println("HelloR");
+        System.out.println("HelloR");
+        System.out.println("HelloR");
+        System.out.println("HelloR");
+    }
+}
